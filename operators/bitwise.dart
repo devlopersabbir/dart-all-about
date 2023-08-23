f220dc2cbd529742d4cb9c16216ae1886b33b44b
devlopersabbir <devlopersabbir@gmail.com>
2023-08-23 16:12:39 +0600

    Bitwise operator

--- operators/bitwise.dart
+++ operators/bitwise.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  // Bitwise Operator
+  /**
+   * Bitwise AND                  &
+   * Bitwise OR                   |
+   * Bitwise XOR                  ^
+   * Bitwise NOT                  ~
+   * Left shift                   <<
+   * Right shift                  >>
+   */
+
+  int fnum = 12;
+  int lnum = 10;
+  int mnum;
+
+  var result = fnum & lnum;
+  print(result);
+}
